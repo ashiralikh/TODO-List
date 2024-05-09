991afd2b026b509cd05d3a6b6af8438489fa5228
ashiralikh <kaashir218@gmail.com>
2024-05-10 02:32:41 +0500

    todo list

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condtion = true;
+while (condtion) {
+    let addtask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what do you want to Add in your todo?",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false"
+        }
+    ]);
+    todos.push(addtask.todo);
+    condtion = addtask.addMore;
+    console.log(todos);
+}
